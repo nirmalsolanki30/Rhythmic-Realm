7c954cc9b2d07b7c4ca9cad884bf71263998c394
nirmalsolanki30 <solanki.nirmal847@gmail.com>
2024-01-24 01:07:04 +0530

    continued progress on login authentication

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("dotenv/config")
 
 const cors = require("cors");
 const {default : mongoose} = require("mongoose");
@@ -10,9 +11,16 @@ app.get("/",(req,res) => {
     return res.json("hello there......")
 })
 
+//user authentication route
+const userRoute = require("./routes/auth");
+app.use("/api/users/", userRoute);
 
-mongoose.connect(process.envDB_STRING, {useNewUrlParser : true});
+
+mongoose.connect(process.env.DB_STRING, {useNewUrlParser : true});
 mongoose.connection
 .once("open" , ()=>console.log())
+.on("error", (error)=>{
+    console.log(`Error : ${error}`);
+} )
 
 app.listen(3000, ()=> console.log("Listening to the port 3000"));
---
+++ server/routes/auth.js
@@ -0,0 +1,54 @@
+const router = require("express").Router();
+const user = require("../models/user")
+const admin = require("../config/firebase.config");
+
+router.get("/login", async(req,res) => {
+    if(!req.headers.authorization)
+    {
+        return res.status(500).send({message : "Invalid Token"});
+    }
+
+    const token = req.headers.authorization.split(" ")[1];
+
+    try{
+        const decodeValue = await admin.auth().verifyIdToken(token);
+        if(!decodeValue){
+            return res.status(505).json({message : "Un Authorized"})
+        }
+        else{
+            //Checking user exists or not
+            const userExists = await user.findOne({"user_id" : decodeValue.user_id})
+            if(!userExists){
+                newUserData(decodeValue , req ,res)
+            }
+            else{
+                return res.send("Need to update")
+            }
+
+        }
+    }catch(error){
+        return res.status(505).json({message : error})
+    }  
+}) 
+
+const newUserData = async (decodeValue, req, res) => {
+    const newUser = new user({
+        name :decodeValue.name,
+        email : decodeValue.email,
+        imageURL : decodeValue.picture,
+        user_id : decodeValue.user_id,
+        email_verified : decodeValue.email_verified,
+        role : "member",
+        auth_time : decodeValue.auth_time,
+    })
+
+    try{
+        const savedUser = await newUser.save();
+        res.status(200).send({user : savedUser})
+    }catch (error) {
+        res.status(400).send({success : false, msg : error});
+    }
+    
+}
+
+module.exports = router;
