d32fe71f78f83ff9ac2f773a2ec8430ccc735c59
nirmalsolanki30 <solanki.nirmal847@gmail.com>
2024-02-20 22:28:42 +0530

    worked on music player

--- clientvite/src/components/MusicPlayer.jsx
+++ clientvite/src/components/MusicPlayer.jsx
@@ -1,15 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useStateValue} from '../context/StateProvider';
 import {motion} from "framer-motion";
 import { RiPlayListFill } from "react-icons/ri";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
+import { getAllSongs } from '../api';
+import { actionType } from '../context/reducer';
+import { IoMdClose } from "react-icons/io";
+import { IoArrowRedo, IoArrowUndo, IoMusicalNote } from "react-icons/io5";
 
 const MusicPlayer = () => {
-  const [{ allSongs, songIndex, isSongPlaying }, dispatch] =
-    useStateValue();
+  const [{ allSongs, songIndex, isSongPlaying }, dispatch] =useStateValue();
+
+  const closeMusicPlayer = () => {
+    if (isSongPlaying) {
+      dispatch({
+        type: actionType.SET_ISSONG_PLAYING,
+        isSongPlaying: false,
+      });
+    }
+  };
+
+  const nextTrack = () => {
+    if (songIndex === allSongs.length-1) {
+      dispatch({
+        type: actionType.SET_SONG_INDEX,
+        songIndex: 0,
+      });
+    } else {
+      dispatch({
+        type: actionType.SET_SONG_INDEX,
+        songIndex: songIndex + 1,
+      });
+    }
+  };
+
+  const previousTrack = () => {
+    if (songIndex === 0) {
+      dispatch({
+        type: actionType.SET_SONG_INDEX,
+        songIndex: 0,
+      });
+    } else {
+      dispatch({
+        type: actionType.SET_SONG_INDEX,
+        songIndex: songIndex - 1,
+      });
+    }
+  };
+
+
+  const [isPlayList,setIsPlayList] = useState(false)
   return (
-    <div className='w-full full flex items-center gap-3 overflow-hidden'>
+    <div className='w-full full flex items-center gap-3'>
       <div className={`w-full full items-center gap-3 p-4 flex relative`}>
         <img
           src={allSongs[songIndex]?.imageURL}
@@ -32,7 +75,7 @@ const MusicPlayer = () => {
           </p>
           <motion.i
             whileTap={{ scale: 0.8 }}
-            //onClick={() => setIsPlayList(!isPlayList)}
+            onClick={() => setIsPlayList(!isPlayList)}
           >
             <RiPlayListFill className="text-textColor hover:text-headingColor text-3xl cursor-pointer" />
           </motion.i>
@@ -44,13 +87,89 @@ const MusicPlayer = () => {
             onPlay={() => console.log("is playing")}
             autoPlay={true}
             showSkipControls={true}
-            //onClickNext={nextTrack}
-            //onClickPrevious={previousTrack}
+            onClickNext={nextTrack}
+            onClickPrevious={previousTrack}
           />
         </div>
+
+        {
+          isPlayList && (
+            <PlayListCard />
+          )
+        }
+        <IoMdClose onClick={closeMusicPlayer} />
       </div>
     </div>
   )
 }
+
+export const PlayListCard = () =>{
+  const [{ allSongs, songIndex, isSongPlaying }, dispatch] =  useStateValue();
+
+  useEffect(() => {
+    if (!allSongs) {
+      getAllSongs().then((data) => {
+        dispatch({
+          type: actionType.SET_ALL_SONGS,
+          allSongs: data.songs,
+        });
+      });
+    }
+  }, []);
+
+  const setCurrentPlaySong = (si) => {
+    if (!isSongPlaying) {
+      dispatch({
+        type: actionType.SET_ISSONG_PLAYING,
+        isSongPlaying: true,
+      });
+    }
+    if (songIndex !== si) {
+      dispatch({
+        type: actionType.SET_SONG_INDEX,
+        songIndex: si,
+      });
+    }
+  };
+
+
+  return (
+    <div className="absolute left-4 bottom-24 gap-2 py-2 w-350 max-w-[350px] h-510 max-h-[510px] flex flex-col overflow-y-scroll scrollbar-thin rounded-md shadow-md bg-primary">
+      {allSongs.length > 0 ? (
+        allSongs.map((music, index) => (
+          <motion.div
+            initial={{ opacity: 0, translateX: -50 }}
+            animate={{ opacity: 1, translateX: 0 }}
+            transition={{ duration: 0.3, delay: index * 0.1 }}
+            className={`group w-full p-4 hover:bg-card flex gap-3 items-center cursor-pointer bg-transparen `}
+            onClick={() => setCurrentPlaySong(index)}
+            key={index}
+          >
+            <IoMusicalNote className="text-textColor group-hover:text-headingColor text-2xl cursor-pointer" />
+
+            <div className="flex items-start flex-col">
+              <p className="text-lg text-headingColor font-semibold">
+                {`${
+                  music?.name.length > 20
+                    ? music?.name.slice(0, 20)
+                    : music?.name
+                }`}{" "}
+                <span className="text-base">({music?.album})</span>
+              </p>
+              <p className="text-textColor">
+                {music?.artist}{" "}
+                <span className="text-sm text-textColor font-semibold">
+                  ({music?.category})
+                </span>
+              </p>
+            </div>
+          </motion.div>
+        ))
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
 
 export default MusicPlayer
